efc6425993ba5de77e8f41f13c16a958bfcc2ef0
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-03 22:37:22 +0100

    Reading All The Fields from the form

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 document.querySelector('#generate-names').addEventListener('submit',loadNames);
 
 // Execute the function to query the Api
-
+/**
+ * @param  {[object]}
+ * @return {[type]}
+ */
 function loadNames(e) {
    e.preventDefault();
 
@@ -10,8 +13,8 @@ function loadNames(e) {
   const origin = document.getElementById("country").value;
   const genre = document.getElementById('genre').value;
   const amount = document.getElementById('quantity').value;
-   
-   // Build the URL 
+
+   // Build the URL
 
    let url = 'https://uinames.com/api/?';
 
@@ -21,6 +24,22 @@ function loadNames(e) {
 
     	url += `region=${origin}&`;
     }
+
+
+  // Read the genre and append to the url
+
+    if(genre !==''){
+
+    	url += `gender=${genre}&`;
+    }
+
+  // Read the amount and append to the url
+
+    if(amount !==''){
+
+    	url += `amount=${amount}&`;
+    }
+
 
   console.log(url);
 }
