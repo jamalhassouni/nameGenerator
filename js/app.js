231c5fa8aaa48f57c5c950f3c7dfa935a2cebe65
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-04 12:07:57 +0100

    refactor:Using fetch API instead of Ajax

--- js/app.js
+++ js/app.js
@@ -1,74 +1,57 @@
-document.querySelector('#generate-names').addEventListener('submit',loadNames);
+document.querySelector("#generate-names").addEventListener("submit", loadNames);
 
 // Execute the function to query the Api
-/**
- * @param  {[object]}
- * @return {[type]}
- */
+
 function loadNames(e) {
-   e.preventDefault();
+	e.preventDefault();
 
-   // Read the values from the form and create the variables
+	// Read the values from the form and create the variables
 
-  const origin = document.getElementById("country").value;
-  const genre = document.getElementById('genre').value;
-  const amount = document.getElementById('quantity').value;
+	const origin = document.getElementById("country").value;
+	const genre = document.getElementById("genre").value;
+	const amount = document.getElementById("quantity").value;
 
-   // Build the URL
+	// Build the URL
 
-   let url = 'https://uinames.com/api/?';
+	let url = "https://uinames.com/api/?";
 
-   // Read the origin and append to the url
+	// Read the origin and append to the url
 
-    if(origin !==''){
+	if (origin !== "") {
+		url += `region=${origin}&`;
+	}
 
-    	url += `region=${origin}&`;
-    }
+	// Read the genre and append to the url
 
+	if (genre !== "") {
+		url += `gender=${genre}&`;
+	}
 
-  // Read the genre and append to the url
+	// Read the amount and append to the url
 
-    if(genre !==''){
+	if (amount !== "") {
+		url += `amount=${amount}&`;
+	}
 
-    	url += `gender=${genre}&`;
-    }
-
-  // Read the amount and append to the url
-
-    if(amount !==''){
-
-    	url += `amount=${amount}&`;
-    }
-
-
- // Ajax Call
- const  xhr= new XMLHttpRequest();
-
- //  Open the connection
-   xhr.open('GET', url,true);
-
-   //  Execute the function
-   xhr.onload = function () {
-
-   	   if(this.status === 200) {
-           const  names = JSON.parse(this.responseText);
-
-           // Insert into the HTML
-           let html = '<h2>Generated Names</h2>';
-            html+= '<ul class="list">';
-             names.forEach(function(data){
-               html+=`
+	// Fetch Api
+	fetch(url)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(names) {
+			let html = "<h2>Generated Names</h2>";
+			html += '<ul class="list">';
+			names.forEach(function(data) {
+				html += `
                  <li>${data.name}</li>
-               `;
+              `;
+			});
 
-             })
+			html += "</ul>";
 
-            html +='</ul>';
-          document.querySelector('#result').innerHTML = html;
-   	   }
-   }
-
-     // Send the  request
-    xhr.send();
+			document.querySelector("#result").innerHTML = html;
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
 }
-
